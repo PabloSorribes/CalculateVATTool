322c76d74e0152341bf73b44d3ed85d840b9b079
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2019-08-18 21:01:07 +0200

    clearer code

--- Assets/MomsCalculator.cs
+++ Assets/MomsCalculator.cs
@@ -3,14 +3,19 @@
 public class MomsCalculator : MonoBehaviour
 {
 	//--- PROCENTSATSER(NETTO VS BRUTTO) ---
-	//NETTO BRUTTO
+	//NETTO		BRUTTO
 	//25%		20%
 	//12%		10,71%
 	//6%		5,66%
 
-	public float netto, nettoProcent;
+	public float netto;
+	[Tooltip("Normal values: 25%, 12% & 6%")]
+	public float nettoProcent;
 	public float NettoProcent { get { return nettoProcent / 100; } }
-	public float brutto, bruttoProcent;
+
+	public float brutto;
+	[Tooltip("Normal values: 20%, 10.71% & 5.66%")]
+	public float bruttoProcent;
 	public float BruttoProcent { get { return bruttoProcent / 100; } }
 
 	private void Update()
@@ -18,26 +23,26 @@ public class MomsCalculator : MonoBehaviour
 		if (Input.GetKeyDown(KeyCode.Alpha1))
 		{
 			Debug.Log("Netto " + netto + ", Brutto " + brutto + "\n" +
-				"<color=yellow>Brutto from Netto: " + CalculateBruttoFromNetto() + "</color>");
+				"<color=yellow>Brutto based on Netto: " + CalculateBruttoFromNetto() + "</color>");
 		}
 		if (Input.GetKeyDown(KeyCode.Alpha2))
 		{
 			Debug.Log("Netto " + netto + ", Brutto " + brutto + "\n" +
-				"<color=blue>Netto from Brutto: " + CalculateNettoFromBrutto() + "</color>");
+				"<color=blue>Netto based on Brutto: " + CalculateNettoFromBrutto() + "</color>");
 		}
 	}
 
 	private float CalculateBruttoFromNetto()
 	{
 		brutto = netto + (netto * NettoProcent);
-		Mathf.Round(brutto);
+		brutto = Mathf.Round(brutto);
 		return brutto;
 	}
 
 	private float CalculateNettoFromBrutto()
 	{
 		netto = brutto + (brutto * -BruttoProcent);
-		Mathf.Round(netto);
+		netto = Mathf.Round(netto);
 		return netto;
 	}
 }
