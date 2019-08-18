1037ff45c712205e0261d5ec24be8414748e271f
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2019-08-19 00:39:38 +0200

    Basically coded the entire thing

--- Assets/MomsCalculatorGUI.cs
+++ Assets/MomsCalculatorGUI.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class MomsCalculatorGUI : MonoBehaviour
 {
@@ -8,41 +10,77 @@ public class MomsCalculatorGUI : MonoBehaviour
 	//12%		10,71%
 	//6%		5,66%
 
-	public float netto;
-	[Tooltip("Normal values: 25%, 12% & 6%")]
-	public float nettoProcent;
-	public float NettoProcent { get { return nettoProcent / 100; } }
+	[SerializeField] private Dropdown nettoPercent;
+	[SerializeField] private Dropdown bruttoPercent;
+	private readonly float[] possibleNettoPercentages = { 6.0f, 12.0f, 25.0f };
+	private readonly float[] possibleBruttoPercentages = { 5.66f, 10.71f, 20.0f };
 
-	public float brutto;
-	[Tooltip("Normal values: 20%, 10.71% & 5.66%")]
-	public float bruttoProcent;
-	public float BruttoProcent { get { return bruttoProcent / 100; } }
+	[Space]
 
-	private void Update()
+	[SerializeField] private InputField nettoMoney;
+	[SerializeField] private InputField bruttoMoney;
+
+	private float currentNettoPercent;
+	public float CurrentNettoPercent { get { return currentNettoPercent / 100; } }
+
+	private float currentBruttoProcent;
+	public float CurrentBruttoProcent { get { return currentBruttoProcent / 100; } }
+
+	private void Start()
 	{
-		if (Input.GetKeyDown(KeyCode.Alpha1))
-		{
-			Debug.Log($"Netto {netto}, Brutto {brutto}\n" +
-				$"<color=yellow>Brutto based on Netto: {CalculateBruttoFromNetto()}</color>");
-		}
-		if (Input.GetKeyDown(KeyCode.Alpha2))
-		{
-			Debug.Log($"Netto {netto}, Brutto {brutto}\n" +
-				$"<color=blue>Netto based on Brutto:{CalculateNettoFromBrutto()}</color>");
-		}
+		//Set initial values to the most common percentages (25% vs 20%) and update float-versions of these values.
+		nettoPercent.value = 2;
+		bruttoPercent.value = 2;
+		UpdateCurrentPercentages();
 	}
 
-	private float CalculateBruttoFromNetto()
+	public void OnNettoPercentageChanged()
 	{
-		brutto = netto + (netto * NettoProcent);
-		brutto = Mathf.Round(brutto);
-		return brutto;
+		bruttoPercent.value = nettoPercent.value;
+		UpdateCurrentPercentages();
+		OnBruttoMoneyChanged();
+		OnNettoMoneyChanged();
 	}
 
-	private float CalculateNettoFromBrutto()
+	public void OnBruttoPercentageChanged()
 	{
-		netto = brutto + (brutto * -BruttoProcent);
-		netto = Mathf.Round(netto);
-		return netto;
+		nettoPercent.value = bruttoPercent.value;
+		UpdateCurrentPercentages();
+		OnNettoMoneyChanged();
+		OnBruttoMoneyChanged();
+	}
+
+	private void UpdateCurrentPercentages()
+	{
+		currentNettoPercent = possibleNettoPercentages[nettoPercent.value];
+		currentBruttoProcent = possibleBruttoPercentages[bruttoPercent.value];
+	}
+
+	public void OnNettoMoneyChanged()
+	{
+		float currentNetto = float.Parse(nettoMoney.text, CultureInfo.InvariantCulture.NumberFormat);
+		float currentBrutto = CalculateBruttoFromNetto(currentNetto, CurrentNettoPercent);
+		bruttoMoney.text = currentBrutto.ToString();
+	}
+
+	public void OnBruttoMoneyChanged()
+	{
+		float currentBrutto = float.Parse(bruttoMoney.text, CultureInfo.InvariantCulture.NumberFormat);
+		float currentNetto = CalculateNettoFromBrutto(currentBrutto, CurrentBruttoProcent);
+		nettoMoney.text = currentNetto.ToString();
+	}
+
+	private float CalculateBruttoFromNetto(float nettoMoney, float nettoPercentage)
+	{
+		float bruttoMoney = nettoMoney + (nettoMoney * nettoPercentage);
+		bruttoMoney = Mathf.Round(bruttoMoney);
+		return bruttoMoney;
+	}
+
+	private float CalculateNettoFromBrutto(float bruttoMoney, float bruttoPercentage)
+	{
+		float nettoMoney = bruttoMoney + (bruttoMoney * -bruttoPercentage);
+		nettoMoney = Mathf.Round(nettoMoney);
+		return nettoMoney;
 	}
 }
