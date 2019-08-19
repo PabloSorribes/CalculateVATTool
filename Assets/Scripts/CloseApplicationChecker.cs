6ec2eaabe1f21f6d68e378f945676939573196b0
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2019-08-19 03:26:41 +0200

    Implemented quit screen

---
+++ Assets/Scripts/CloseApplicationChecker.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class CloseApplicationChecker : MonoBehaviour
+{
+	[SerializeField] private GameObject closeApplicationUI;
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape) && !closeApplicationUI.activeSelf)
+		{
+			ToggleCloseApplicationUI(true);
+		}
+		else if (Input.GetKeyDown(KeyCode.Escape) && closeApplicationUI.activeSelf)
+		{
+			ToggleCloseApplicationUI(false);
+		}
+		else if (closeApplicationUI.activeSelf && (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter)))
+		{
+			CloseApplication();
+		}
+	}
+
+	public void ToggleCloseApplicationUI(bool activate)
+	{
+		closeApplicationUI.SetActive(activate);
+	}
+
+	public void CloseApplication()
+	{
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+	Application.OpenURL(webplayerQuitURL);
+#else
+	Application.Quit();
+#endif
+	}
+}
